5cdcb8f14964ef692410e93641dbd88ea925cfcd
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-09-23 16:21:28 +0000

    checking arg from cli

--- pylack.zsh
+++ pylack.zsh
@@ -1,3 +1,26 @@
 #!/usr/bin/zsh
 
-echo `man python3`
+__usage="
+USAGE
+
+pylack <command> [OPTIONS]
+
+Options:
+  -h, --help                   Something something something help
+  -v, --version                Something something something version
+
+  startproject                 To start a new project.
+"
+
+# path to main python executable script
+path = "./pylack/main.py"
+main_script=$(dirname "$path")
+
+# Check if two arguments are provided
+if [ $# -eq 2 ] && [ "$1" = "startproject" ]; then
+    project_name="$2"
+    activate=$(python3 $main_script "$project_name")
+    echo "$activate"
+else
+    echo "$__usage"
+fi
